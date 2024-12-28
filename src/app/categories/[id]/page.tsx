5631781fd1bec0e748c434a51a4e3faebc9a8b39
ibousarr <sarrsindian@gmail.com>
2024-12-28 19:12:50 +0000

    markdown skill

---
+++ src/app/categories/[id]/page.tsx
@@ -0,0 +1,9 @@
+
+import React from 'react'
+
+export default async function CatSinglePage() {
+  return (
+    <div>CatSinglePage</div>
+  )
+}
+
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+          className="container"
         >
           <header>
             <SignedOut>
